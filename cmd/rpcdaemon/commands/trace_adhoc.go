6586094d50a27ad4cbae1276f1d8ba21b740a4da
Ben Gelb <ben.gelb@gmail.com>
2022-01-15 09:26:52 -0800

    plumb error into trace_call(many) outer json response

--- cmd/rpcdaemon/commands/trace_adhoc.go
+++ cmd/rpcdaemon/commands/trace_adhoc.go
@@ -62,6 +62,7 @@ type TraceCallParam struct {
 type TraceCallResult struct {
 	Output          hexutil.Bytes                           `json:"output"`
 	UsedGas         hexutil.Uint64                          `json:"gasUsed"`
+	Error           string                                  `json:"error,omitempty"`
 	StateDiff       map[libcommon.Address]*StateDiffAccount `json:"stateDiff"`
 	Trace           []*ParityTrace                          `json:"trace"`
 	VmTrace         *VmTrace                                `json:"vmTrace"`
@@ -799,6 +800,7 @@ func (api *TraceAPIImpl) ReplayTransaction(ctx context.Context, txHash libcommon
 		if txno == int(txnIndex) {
 			result.Output = trace.Output
 			result.UsedGas = trace.UsedGas
+			result.Error = trace.Error
 			if traceTypeTrace {
 				result.Trace = trace.Trace
 			}
@@ -868,6 +870,7 @@ func (api *TraceAPIImpl) ReplayBlockTransactions(ctx context.Context, blockNrOrH
 		tr := &TraceCallResult{}
 		tr.Output = trace.Output
 		tr.UsedGas = trace.UsedGas
+		tr.Error = trace.Error
 		if traceTypeTrace {
 			tr.Trace = trace.Trace
 		} else {
@@ -1012,6 +1015,11 @@ func (api *TraceAPIImpl) Call(ctx context.Context, args TraceCallParam, traceTyp
 	}
 	traceResult.Output = common.CopyBytes(execResult.ReturnData)
 	traceResult.UsedGas = hexutil.Uint64(execResult.UsedGas)
+	var errString string
+	if execResult.Err != nil {
+		errString = execResult.Err.Error()
+	}
+	traceResult.Error = errString
 	if traceTypeAccessList {
 		ot.r.AccessList = ot.al.AccessList()
 	}
@@ -1254,6 +1262,11 @@ func (api *TraceAPIImpl) doCallMany(ctx context.Context, dbtx kv.Tx, msgs []type
 		}
 		traceResult.Output = common.CopyBytes(execResult.ReturnData)
 		traceResult.UsedGas = hexutil.Uint64(execResult.UsedGas)
+		var errString string
+		if execResult.Err != nil {
+			errString = execResult.Err.Error()
+		}
+		traceResult.Error = errString
 		if traceTypeAccessList {
 			ot.r.AccessList = ot.al.AccessList()
 		}

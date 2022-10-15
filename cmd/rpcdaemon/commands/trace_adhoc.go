37b7ef1036b87d281702d63e7d51e84c628e3953
Ben Gelb <ben.gelb@gmail.com>
2022-10-14 17:18:51 -0700

    remove maxgaslimit from trace_call and ilk
    remove maxgaslimit from trace_call and ilk

--- cmd/rpcdaemon/commands/trace_adhoc.go
+++ cmd/rpcdaemon/commands/trace_adhoc.go
@@ -989,9 +989,6 @@ func (api *TraceAPIImpl) Call(ctx context.Context, args TraceCallParam, traceTyp
 	blockCtx := transactions.NewEVMBlockContext(engine, header, blockNrOrHash.RequireCanonical, tx, api._blockReader)
 	txCtx := core.NewEVMTxContext(msg)
 
-	blockCtx.GasLimit = math.MaxUint64
-	blockCtx.MaxGasLimit = true
-
 	// Increment the BlockNumber and Time values to simulate the transaction of
 	// interest in the next (N+1) block instead of the current (already mined) one
 	blockCtx.Time += 12
@@ -1177,10 +1174,8 @@ func (api *TraceAPIImpl) doCallMany(ctx context.Context, dbtx kv.Tx, msgs []type
 	defer cancel()
 	results := []*TraceCallResult{}
 
-	useParent := false
 	if header == nil {
 		header = parentHeader
-		useParent = true
 	}
 
 	for txIndex, msg := range msgs {
@@ -1234,10 +1229,6 @@ func (api *TraceAPIImpl) doCallMany(ctx context.Context, dbtx kv.Tx, msgs []type
 			blockCtx.BlockNumber += 1
 		}
 
-		if useParent {
-			blockCtx.GasLimit = math.MaxUint64
-			blockCtx.MaxGasLimit = true
-		}
 		ibs.Reset()
 		// Create initial IntraBlockState, we will compare it with ibs (IntraBlockState after the transaction)
 
